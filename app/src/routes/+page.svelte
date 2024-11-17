be8fcf5e0d9b9b1be32c55119145869967d4afd0
Elkeno Jones <elkeno.jones@binarystar.ca>
2024-11-16 23:25:49 -0400

    fetch data from drupal

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,18 @@
+<script>
+    import { onMount } from "svelte";
+
+    let baseUrl = import.meta.env.VITE_API;
+    let test;
+  onMount(async () => {
+    console.log('onMount');
+    // const client = new JsonApiClient("http://mindgames_cms:8086");
+    // test = await client.getCollection("node--idea");
+    test = await fetch(`${baseUrl}/api/mindgames`);
+    console.log(test.json());
+  });
+</script>
+
+
+<hr />
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
